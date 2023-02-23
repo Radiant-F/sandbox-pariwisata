ac912b89fe77bd556b182b9147201d730c6ee11a
Radiant-F <radiantfadilah0@gmail.com>
2023-02-23 20:18:25 +0700

    remove unecessary extra reducer

--- src/redux/slices/testSlice.test.js
+++ src/redux/slices/testSlice.test.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchSignIn } from "../../features/__test__/services/testServices";
 
 const initialState = {
   status: "idle",
@@ -14,19 +13,7 @@ export const testSlice = createSlice({
       state.status = action.payload;
     },
   },
-  extraReducers(builder) {
-    builder
-      .addCase(fetchSignIn.pending, (state, action) => {
-        state.status = "pending";
-      })
-      .addCase(fetchSignIn.fulfilled, (state, action) => {
-        state.status = "success";
-        state.data = action.payload;
-      })
-      .addCase(fetchSignIn.rejected, (state, action) => {
-        state.status = "failed";
-      });
-  },
+  extraReducers(builder) {},
 });
 
 export const { SetStatusTest } = testSlice.actions;
