80ef33a9b92bd326143d2f1e30807024993cd4e8
Radiant-F <radiantfadilah0@gmail.com>
2023-02-23 20:16:57 +0700

    add import

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from "./Home/Home";
+import Missing from "./Missing/Missing";
+import Tourist from "./Tourist/Tourist";
+import UserTourist from "./Tourist/UserTourist";
+import PasswordRecovery from "./PasswordRecovery/PasswordRecovery";
+import Profile from "./Profile/Profile";
+import LibsDemo from "./__test__/LibsDemo.test";
+
+export {
+  Home,
+  Tourist,
+  Profile,
+  Missing,
+  LibsDemo,
+  PasswordRecovery,
+  UserTourist,
+};
