e54e449cb0dce8eda0a5eb17df53eee363443117
Radiant-F <radiantfadilah0@gmail.com>
2023-02-23 20:11:57 +0700

    comment

--- src/features/__test__/services/testServices.js
+++ src/features/__test__/services/testServices.js
@@ -1,26 +1,26 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { getAPIinfo, postSignIn } from "../../../services/services";
+// import { createAsyncThunk } from "@reduxjs/toolkit";
+// import { getAPIinfo, postSignIn } from "../../../services/services";
 
-export const getTestServices = createAsyncThunk(
-  "getTestServices",
-  async (params, { dispatch, getState }) => {
-    try {
-      const { data } = await getAPIinfo();
-      return data;
-    } catch (error) {
-      return error.message;
-    }
-  }
-);
+// export const getTestServices = createAsyncThunk(
+//   "getTestServices",
+//   async (params, { dispatch, getState }) => {
+//     try {
+//       const { data } = await getAPIinfo();
+//       return data;
+//     } catch (error) {
+//       return error.message;
+//     }
+//   }
+// );
 
-export const fetchSignIn = createAsyncThunk(
-  "fetchSignIn",
-  async (formData, { dispatch }) => {
-    try {
-      const { data } = await postSignIn(formData);
-      return data;
-    } catch (error) {
-      return error.message;
-    }
-  }
-);
+// export const fetchSignIn = createAsyncThunk(
+//   "fetchSignIn",
+//   async (formData, { dispatch }) => {
+//     try {
+//       const { data } = await postSignIn(formData);
+//       return data;
+//     } catch (error) {
+//       return error.message;
+//     }
+//   }
+// );
