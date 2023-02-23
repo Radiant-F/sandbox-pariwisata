c0b8092f848282145dc852cb9e7ee6d1d56a1667
Radiant-F <radiantfadilah0@gmail.com>
2023-02-23 20:17:52 +0700

    add pariwisata & utils reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "./slices/authSlice";
+import authSlice from "../features/Auth/services/authSlice";
+import pariwisataSlice from "./slices/pariwisataSlice";
 import testSlice from "./slices/testSlice.test.js";
+import utilsSlice from "./slices/utilsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     test: testSlice,
+    pariwisata: pariwisataSlice,
+    utils: utilsSlice,
   },
 });
