ac702d0e3807a2dd7a19b85993f11d2429be8301
Radiant-F <radiantfadilah0@gmail.com>
2023-02-23 20:19:24 +0700

    add signin logout user detail recovery sign out

--- src/services/services.js
+++ src/services/services.js
@@ -1,16 +1,31 @@
 import axios from "axios";
 
-const host = "https://manajemen.pondokit.com/api";
+const host = "https://api-entrytest.sandboxindonesia.id/api";
 
 const config = (token) => ({
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
     Accept: "application/json",
-    // "Control-Allow-Origin": "*",
   },
 });
 
-export const getAPIinfo = () => axios.get(`${host}`, config());
+const configUserUpdate = (access) => ({
+  headers: {
+    "Content-Type": "multipart/form-data",
+    Authorization: `Bearer ${access}`,
+  },
+});
+
 export const postSignIn = (formData) =>
-  axios.post(`${host}/login`, formData, config());
+  axios.post(`${host}/auth/login/`, formData, config());
+export const getUserData = (access) =>
+  axios.get(`${host}/user/user/me/`, config(access));
+export const postRecoveryPassword = (formData, access) =>
+  axios.get(`${host}/user/user/change-password/`, formData, config(access));
+export const patchUserData = (formData, access) =>
+  axios.patch(`${host}/user/user/me/`, formData, configUserUpdate(access));
+export const postSignOut = (refresh) =>
+  axios.post(`${host}/auth/logout/`, refresh);
+
+export const getAPIinfo = () => axios.get(`${host}`, config());
