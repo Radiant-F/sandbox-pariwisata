2ecfbb307b7e6158b618abed40bc2fdb3dcfb685
Radiant-F <radiantfadilah0@gmail.com>
2023-02-24 13:24:38 +0700

    update styling & navigate

--- src/pages/PasswordRecovery/PasswordRecovery.jsx
+++ src/pages/PasswordRecovery/PasswordRecovery.jsx
@@ -1,10 +1,13 @@
-import axios from "axios";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Button, Gap } from "../../assets";
 import { fetchRecoverPassword } from "../../features/Auth/services/authServices";
+import styles from "./index.module.css";
 
 export default function PasswordRecovery() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [prevPassword, setPrevPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmNewPass, setConfirmNewPass] = useState("");
@@ -20,40 +23,59 @@ export default function PasswordRecovery() {
       old_password: prevPassword,
       new_password: newPassword,
     };
-    // console.log(formData);
     dispatch(fetchRecoverPassword(formData));
   }
 
   return (
-    <main>
-      <div>PasswordRecovery</div>
-      <h3>Password Lama</h3>
+    <main className={styles.container}>
+      <div className={styles.textTitle}>Ubah Password</div>
+      <Gap height={50} />
+      <div style={{ fontWeight: "bold" }}>Password Lama</div>
       <input
+        className={styles.inputContainer}
+        placeholder={"Masukan password lama"}
         title="password"
         type={"password"}
         onChange={(event) => setPrevPassword(event.target.value)}
       />
       <br />
-      <h3>Password Baru</h3>
+      <Gap height={20} />
+      <div style={{ fontWeight: "bold" }}>Password Baru</div>
       <input
+        className={styles.inputContainer}
+        placeholder={"Masukan password baru"}
         title="password"
         type={"password"}
         onChange={(event) => setNewPassword(event.target.value)}
       />
       <br />
-      <h3>Konfirmasi Password Baru</h3>
+      <Gap height={20} />
+      <div style={{ fontWeight: "bold" }}>Konfirmasi Password Baru</div>
       <input
+        className={styles.inputContainer}
+        placeholder={"Masukan konfirmasi password baru"}
         title="password"
         type={"password"}
         onChange={(event) => setConfirmNewPass(event.target.value)}
       />
       <br />
-      {newPassword !== confirmNewPass && <div>Password tidak cocok</div>}
-      <br />
-      <button>Batal</button>
-      <button disabled={disableSubmit} onClick={submitRecovery}>
-        Simpan
-      </button>
+      {/* {newPassword !== confirmNewPass && <div>Password tidak cocok</div>} */}
+      <Gap height={50} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+      >
+        <Button title="Batal" onClick={() => navigate("/profile")} cancel />
+        <Gap width={10} />
+        <Button
+          title="Simpan"
+          disabled={disableSubmit}
+          onClick={submitRecovery}
+          width={120}
+        />
+      </div>
     </main>
   );
 }
