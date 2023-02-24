ac9703157691ce1c7b317e9de4e431a45bb94103
Radiant-F <radiantfadilah0@gmail.com>
2023-02-24 18:10:04 +0700

    update promise & add fetch

--- src/features/UserTourist/services/userTouristServices.js
+++ src/features/UserTourist/services/userTouristServices.js
@@ -1,15 +1,33 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { postTouristObject } from "../../../services/services";
+import { SetBorderline } from "../../../redux/slices/touristObjSlice";
+import { getBorderline, postTouristObject } from "../../../services/services";
 
 export const fetchAddUserTourist = createAsyncThunk(
   "userTourist",
   async (formData, { getState, dispatch }) => {
+    const { access } = getState().auth.token;
+    console.log("body data wisata: ", formData);
     try {
-      const { data } = await postTouristObject(formData);
+      const { data } = await postTouristObject(formData, access);
       console.log("Berhasil menambah obj wisata:", data);
       return data;
     } catch (error) {
-      console.log("Gagal menambah obj wisata masbro:", error.message);
+      console.log("Gagal menambah obj wisata masbro:", error.response);
+      return error.message;
+    }
+  }
+);
+
+export const fetchMapBorder = createAsyncThunk(
+  "fetchMapBorder",
+  async (args, { getState, dispatch }) => {
+    const { access } = getState().auth.token;
+    try {
+      const { data } = await getBorderline(access);
+      dispatch(SetBorderline(data.data));
+      return data;
+    } catch (error) {
+      console.log("gagal data borderline:", error.response);
       return error.message;
     }
   }
