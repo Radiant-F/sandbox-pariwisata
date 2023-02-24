5b4ff697c258c76b8e5aa75de48c27ae631133a7
Radiant-F <radiantfadilah0@gmail.com>
2023-02-24 18:08:51 +0700

    update styling

--- src/assets/components/ButtonAction/ButtonAction.js
+++ src/assets/components/ButtonAction/ButtonAction.js
@@ -4,7 +4,7 @@ import styles from "./index.module.css";
 export default function ButtonAction({ onClick, title, disabled }) {
   return (
     <button className={styles.container} onClick={onClick} disabled={disabled}>
-      <p>{title}</p>
+      <p style={{ color: "white", fontWeight: "bold" }}>{title}</p>
     </button>
   );
 }
