388d654aaf9689c131b365cd94e26a9c7297cf0d
Radiant-F <radiantfadilah0@gmail.com>
2023-02-24 13:24:59 +0700

    update reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "../features/Auth/services/authSlice";
-import pariwisataSlice from "./slices/pariwisataSlice";
-import testSlice from "./slices/testSlice.test.js";
+import authSlice from "./slices/authSlice";
+import touristObjSlice from "./slices/touristObjSlice";
 import utilsSlice from "./slices/utilsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
-    test: testSlice,
-    pariwisata: pariwisataSlice,
+    tourist_obj: touristObjSlice,
     utils: utilsSlice,
   },
 });
