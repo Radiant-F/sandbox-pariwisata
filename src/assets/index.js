74e3163e12d072fdc61feb3a217a1669a5e94508
Radiant-F <radiantfadilah0@gmail.com>
2023-02-24 13:20:18 +0700

    update assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,27 @@
 import IMGpfpDefault from "./images/pfp-default.jpg";
 import IMGsandbox from "./images/logo-sandbox.png";
 import Button from "./components/Button/Button";
+import ButtonAction from "./components/ButtonAction/ButtonAction";
+import IconCloseSquare from "./icons/close-square.png";
+import IconKey from "./icons/key.png";
+import IconEye from "./icons/eye.png";
+import IconUser from "./icons/user-octagon.png";
+import Gap from "./components/Gap";
+import IconChevron from "./icons/chevron.png";
+import IconCamera from "./icons/camera.png";
+import IconAddCircle from "./icons/add-circle.png";
 
-export { IMGpfpDefault, IMGsandbox, Button };
+export {
+  IMGpfpDefault,
+  IMGsandbox,
+  Button,
+  ButtonAction,
+  IconCloseSquare,
+  IconKey,
+  IconEye,
+  IconUser,
+  Gap,
+  IconChevron,
+  IconCamera,
+  IconAddCircle,
+};
