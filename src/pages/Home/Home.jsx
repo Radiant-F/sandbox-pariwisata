4e2e50f66786dcbb7185e0a69c433b3833860d5a
Radiant-F <radiantfadilah0@gmail.com>
2023-02-24 13:24:14 +0700

    update styling & link

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { SetPariwisataId } from "../../redux/slices/pariwisataSlice";
+import { Link } from "react-router-dom";
+import { SetTouristId } from "../../redux/slices/touristObjSlice";
 
 export default function Home() {
   const dispatch = useDispatch();
   return (
     <main>
-      <h1>Home</h1>
+      <h1>Wisata</h1>
       {[...new Array(7).keys()].map((value, index) => (
         <Link
           key={index}
-          to="/pariwisata"
-          onClick={() => dispatch(SetPariwisataId(index))}
+          to="/tourist"
+          onClick={() => dispatch(SetTouristId(index))}
         >
           <div>
-            <h3>Judul Pariwisata {index}</h3>
+            <h3>Dummy Pariwisata {index}</h3>
           </div>
         </Link>
       ))}
