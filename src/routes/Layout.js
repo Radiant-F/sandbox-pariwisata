f232826a542e27d73aa9ea059abfcd3b1f17260c
Radiant-F <radiantfadilah0@gmail.com>
2023-02-24 13:25:08 +0700

    update styling

--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { Gap } from "../assets";
 import { NavigationBar } from "../features/NavigationBar";
 
 function Layout() {
   return (
     <main className="App">
       <NavigationBar />
+      <Gap height={30} />
       <Outlet />
     </main>
   );
