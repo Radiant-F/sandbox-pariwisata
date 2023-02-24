b778ddb9ef71a088dba356c445b90855d3c27795
Radiant-F <radiantfadilah0@gmail.com>
2023-02-24 13:21:57 +0700

    add  styling

--- src/assets/components/Button/Button.js
+++ src/assets/components/Button/Button.js
@@ -1,10 +1,21 @@
 import React from "react";
 import styles from "./index.module.css";
 
-export default function Button({ title = "Login", disabled, loading }) {
+export default function Button({
+  title = "Login",
+  onClick,
+  disabled,
+  width,
+  cancel,
+}) {
   return (
-    <button className={styles.container}>
-      <p>{title}</p>
+    <button
+      className={cancel ? styles.containerCancel : styles.container}
+      style={{ opacity: disabled ? 0.5 : 1, width }}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <p className={cancel ? styles.textCancel : styles.text}>{title}</p>
     </button>
   );
 }
