5221d45d81c44bda12e51f278339338cf992ca90
Radiant-F <radiantfadilah0@gmail.com>
2023-02-24 13:24:49 +0700

    update selector

--- src/pages/Tourist/Tourist.jsx
+++ src/pages/Tourist/Tourist.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export default function Tourist() {
-  const { id } = useSelector((state) => state.pariwisata);
+  const { tourist_id: id } = useSelector((state) => state.tourist_obj);
   return (
     <main>
       <h3>Detail Pariwisata dengan id: {id}</h3>
