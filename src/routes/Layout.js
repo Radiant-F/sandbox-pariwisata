72efd476f8293dd7df7bd8345366a689da6f3f06
Radiant-F <radiantfadilah0@gmail.com>
2023-02-28 13:06:05 +0700

    add footer

--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { Gap } from "../assets";
+import Footer from "../features/Footer/components/Footer";
 import { NavigationBar } from "../features/NavigationBar";
 
 function Layout() {
   return (
     <main className="App">
       <NavigationBar />
-      <Gap height={30} />
+      <Gap height={50} />
       <Outlet />
+      <Gap height={50} />
+      <Footer />
     </main>
   );
 }
