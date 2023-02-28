00cc1ae31ab77dbb8802774d334cfc8764443056
Radiant-F <radiantfadilah0@gmail.com>
2023-02-28 13:06:30 +0700

    update styling

--- src/features/Footer/components/Footer.jsx
+++ src/features/Footer/components/Footer.jsx
@@ -1,7 +1,14 @@
+import { Gap, IMGsandbox, Line } from "../../../assets";
+import styles from "./index.module.css";
+
 export default function Footer() {
   return (
-    <footer>
-      <h1>footer</h1>
+    <footer className={styles.container}>
+      <div className={styles.containerContent}>
+        <img alt="sandbox logo" src={IMGsandbox} style={{ width: 175 }} />
+        <Gap height={20} />
+        <Line />
+      </div>
     </footer>
   );
 }
