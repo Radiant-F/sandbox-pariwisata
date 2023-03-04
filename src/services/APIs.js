456ae730c95fa1f77e5bfe6ba12a0ea9a6b45ab2
Radiant-F <radiantfadilah0@gmail.com>
2023-03-04 15:09:40 +0700

    add post, get request & config

--- src/services/APIs.js
+++ src/services/APIs.js
@@ -1,3 +1,16 @@
 import axios from "axios";
 
 const host = process.env.REACT_APP_API_KEY;
+
+const config = (access) => ({
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Bearer ${access}`,
+  },
+});
+
+export const postSignIn = (formData) =>
+  axios.post(`${host}/auth/login/`, formData, config());
+export const getUserData = (access) =>
+  axios.get(`${host}/user/user/me`, config(access));
