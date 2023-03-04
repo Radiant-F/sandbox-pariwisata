7b036cfed5f62110522e67e1f844c548872d7ec9
Radiant-F <radiantfadilah0@gmail.com>
2023-03-04 15:08:35 +0700

    update import

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import Footer from "./pages/Footer/Footer";
-import NavBar from "./pages/NavBar/NavBar";
 import ButtonAction from "./pages/ButtonAction/ButtonAction";
 import Gap from "./pages/Gap";
 import Line from "./pages/Line";
@@ -7,11 +6,18 @@ import Line from "./pages/Line";
 import IMGsandbox from "./assets/images/logo-sandbox.png";
 import IconPlusRound from "./assets/icons/icon-plus.png";
 import IconUserOctagon from "./assets/icons/user-octagon.png";
-import IconKey from "./assets/icons/user-octagon.png";
+import IconKey from "./assets/icons/key.png";
+import IconEye from "./assets/icons/eye-open.png";
+import IconEyeSlash from "./assets/icons/eye-slash.png";
+import IconCloseSquare from "./assets/icons/close-square.png";
+import IconProfileDefault from "./assets/icons/icon-default.png";
 
 export {
+  IconProfileDefault,
+  IconCloseSquare,
+  IconEye,
+  IconEyeSlash,
   Footer,
-  NavBar,
   IMGsandbox,
   ButtonAction,
   IconPlusRound,
